4b9280ded392d4dfc572be1184d7e5d3c86655d3
Luqman Moton <luqmanmoton@gmail.com>
2026-07-24 19:30:12 -0500

    Updated team and research pages

---
+++ src/data/newsData.js
@@ -0,0 +1,65 @@
+// src/data/newsData.js
+
+export const newsUpdates = [
+    {
+        date: "December 5th 2025",
+        title: "Lupita Graduates!",
+        snippet: "Congrats Dr. Rios!",
+        link: "/team"
+    },
+    {
+        date: "October 28th 2025",
+        title: "Srijani Graduates!",
+        snippet: "Congrats Dr. Biswas!",
+        link: "/team"
+    },
+    {
+        date: "April 20th 2024",
+        title: "Natalia Graduates!",
+        snippet: "Congrats Dr. Rodriguez-Sosa!",
+        link: "/team"
+    },
+    {
+        date: "August 2022",
+        title: "Srijani Wins Award at the Three Minute Thesis Competition (3MT)",
+        snippet: "She won 1st place!",
+        link: "/news"
+    },
+    {
+        date: "May 2022",
+        title: "Srijani wins Award at NGP Poster session",
+        snippet: "She presented a poster talking about the lateral habenula and pessimism",
+        link: "/news"
+    },
+    {
+        date: "November 13th 2023",
+        title: "Grant Awarded",
+        snippet: "Second R01 Award from NIMH",
+        link: "/news"
+    },
+    {
+        date: "April 8th 2021",
+        title: "Grant Awarded",
+        snippet: "Brain Initiative R34 Award",
+        link: "/news"
+    },
+    {
+        date: "Feburary 19th 2021",
+        title: "Grant Awarded",
+        snippet: "Lupita and Natalia are awarded student supplements by NIH",
+        link: "/news"
+    },
+    {
+        date: "March 9th 2020",
+        title: "Grant Awarded",
+        snippet: "First R01 Award from NIMH",
+        link: "/news"
+    },
+    {
+        date: "December 2nd 2018",
+        title: "Grant Awarded",
+        snippet: "Whitehall Foundation Award",
+        link: "/news"
+    }
+
+];
