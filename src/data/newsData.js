e813a9d1fa97fee3e8003444a9507daa1320b0cd
Luqman Moton <luqmanmoton@gmail.com>
2026-07-26 16:11:54 -0500

    Got rid of some read more stuff and hover on publication page

--- src/data/newsData.js
+++ src/data/newsData.js
@@ -5,7 +5,6 @@ export const newsUpdates = [
         date: "December 5th 2025",
         title: "Lupita Graduates!",
         snippet: "Congratulations, Dr. Rios, on earning your Ph.D!",
-        link: "/team"
     },
     {
         date: "November 1st 2025",
@@ -17,7 +16,6 @@ export const newsUpdates = [
         date: "October 28th 2025",
         title: "Srijani Graduates!",
         snippet: "A new doctor in the lab! Congrats Dr. Biswas!",
-        link: "/team"
     },
     {
         date: "June 1st 2025",
@@ -29,49 +27,41 @@ export const newsUpdates = [
         date: "April 20th 2024",
         title: "Natalia Graduates!",
         snippet: "Congrats Natalia! Our first student to graduate. We will miss you!",
-        link: "/team"
     },
     {
         date: "August 2022",
         title: "Srijani Wins Award at the Three Minute Thesis Competition (3MT)",
         snippet: "",
-        link: "/news"
     },
     {
         date: "May 2022",
         title: "Srijani wins Award at NGP Poster session",
         snippet: "",
-        link: "/news"
     },
     {
         date: "November 13th 2023",
         title: "Grant Awarded",
         snippet: "Second R01 Award from NIMH",
-        link: "/news"
     },
     {
         date: "April 8th 2021",
         title: "Grant Awarded",
         snippet: "Brain Initiative R34 Award",
-        link: "/news"
     },
     {
         date: "Feburary 19th 2021",
         title: "Grant Awarded",
         snippet: "Lupita and Natalia are awarded student supplements by NIH",
-        link: "/news"
     },
     {
         date: "March 9th 2020",
         title: "Grant Awarded",
         snippet: "First R01 Award from NIMH",
-        link: "/news"
     },
     {
         date: "December 2nd 2018",
         title: "Grant Awarded",
         snippet: "Whitehall Foundation Award",
-        link: "/news"
     }
 
 ];
