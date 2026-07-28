26222562c1e84920f9e67d50774e8c648b2e1a99
Luqman Moton <luqmanmoton@gmail.com>
2026-07-28 13:44:04 -0500

    fixed error in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.shabellab.org/'
+  site: 'https://www.shabellab.org/',
   integrations: [sitemap()]
 });
